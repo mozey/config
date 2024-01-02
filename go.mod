0bea4897e0b178dd4795914ae85e7716bf3b6d74
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-01-02 12:47:23 +0200

    Simpler test lib

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/mozey/config
 go 1.18
 
 require (
+	github.com/matryer/is v1.4.1
 	github.com/mozey/logutil v0.0.0-20201020133158-b1ec1d809337
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.20.0
 	github.com/stretchr/testify v1.5.1
+	gopkg.in/yaml.v2 v2.2.2
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- pkg/testutil/testutil.go
+++ pkg/testutil/testutil.go
@@ -1,7 +1,13 @@
 package testutil
 
 import (
+	"testing"
+
+	"github.com/matryer/is"
 	config "github.com/mozey/config/pkg/cmdconfig/testdata"
+	"github.com/mozey/logutil"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
 var conf *config.Config
@@ -21,4 +27,40 @@ func Config() (*config.Config, error) {
 	// Copy the struct not the pointer!
 	confCopy := *conf
 	return &confCopy, nil
+}
+
+// I wraps is.I
+type I struct {
+	*is.I
+	t *testing.T
+}
+
+// NoErr logs the error with stack trace if err is not nil and exits
+func (is *I) NoErr(err error) {
+	if err != nil {
+		type stackTracer interface {
+			StackTrace() errors.StackTrace
+		}
+		_, ok := err.(stackTracer)
+		if !ok {
+			// Add stack trace to err if it doesn't have one
+			// See comments in github.com/pkg/errors
+			// "Although the stackTracer interface is not exported by this package,
+			// it is considered a part of its stable public interface"
+			err = errors.WithStack(err)
+		}
+		log.Error().Stack().Err(err).Msg("")
+	}
+	// TODO Behaviour here differs for is.NewRelaxed,
+	// currently this wrapper doesn't support relaxed mode
+	is.I.NoErr(err)
+}
+
+func New(t *testing.T, is *is.I) *I {
+	return &I{t: t, I: is}
+}
+
+func Setup(t *testing.T) *I {
+	logutil.SetupLogger(true)
+	return New(t, is.New(t))
 }
