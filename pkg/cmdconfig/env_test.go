aaba4823d0fe4fe5ac3be9419d45004f03f1c917
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-01-02 14:01:49 +0200

    env

--- pkg/cmdconfig/env_test.go
+++ pkg/cmdconfig/env_test.go
@@ -3,10 +3,12 @@ package cmdconfig
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/mozey/config/pkg/testutil"
 )
 
 func TestUnmarshalENV(t *testing.T) {
+	is := testutil.Setup(t)
+
 	envFileBytes := []byte(`
 	#!/bin/bash xyz
 	
@@ -24,20 +26,22 @@ func TestUnmarshalENV(t *testing.T) {
 	`)
 
 	m, err := UnmarshalENV(envFileBytes)
-	require.NoError(t, err)
-	require.Equal(t, "\"foo\"", m["APP_FOO"])
-	require.Equal(t, "my name is {{.Name}}", m["APP_TEMPLATE"])
-	require.Equal(t, "aws-local", m["AWS_PROFILE"])
+	is.NoErr(err)
+	is.Equal("\"foo\"", m["APP_FOO"])
+	is.Equal("my name is {{.Name}}", m["APP_TEMPLATE"])
+	is.Equal("aws-local", m["AWS_PROFILE"])
 }
 
 func TestMarshalENV(t *testing.T) {
+	is := testutil.Setup(t)
+
 	m := make(map[string]string)
 	m["APP_FOO"] = "\"foo\""
 	m["APP_TEMPLATE"] = "my name is {{.Name}}"
 	m["AWS_PROFILE"] = "aws-local"
 
 	b, err := MarshalENV(m)
-	require.NoError(t, err)
+	is.NoErr(err)
 
 	// TODO Preserve shebang line?
 	envFileBytes := []byte(`APP_FOO="foo"
@@ -45,5 +49,5 @@ APP_TEMPLATE=my name is {{.Name}}
 AWS_PROFILE=aws-local
 `)
 
-	require.Equal(t, envFileBytes, b)
+	is.Equal(envFileBytes, b)
 }
