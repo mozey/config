410d4b65f847a9233d867e365e94c50409c0ea52
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-01-11 16:09:08 +0200

    Bump version

--- cmd/configu/main.go
+++ cmd/configu/main.go
@@ -10,7 +10,7 @@ import (
 // For custom builds, the version can be overwritten with ldflags, see
 // "Golang compile environment variable into binary"
 // https://stackoverflow.com/a/47665780/639133
-var version string = "v0.11.2"
+var version string = "v0.12.0"
 
 func main() {
 	logutil.SetupLogger(true)
