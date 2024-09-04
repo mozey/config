d8a4db9217b523376078509e5b90fbb18cddd1fe
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2024-09-04 11:31:22 +0200

    Default APP_DIR is current working dir

--- pkg/cmdconfig/config.go
+++ pkg/cmdconfig/config.go
@@ -128,19 +128,24 @@ func NewCmdIn(params CmdInParams) *CmdIn {
 // Valid returns true if the command input is valid.
 // It may also set default values
 func (in *CmdIn) Valid() error {
-	// AppDir is required
-	appDirKey := fmt.Sprintf("%sDIR", in.Prefix)
-	appDir := os.Getenv(appDirKey)
-	if appDir == "" {
-		return errors.Errorf("%v env not set%s", appDirKey, "\n")
-	}
-	in.AppDir = appDir
-
 	// Prefix must end with underscore
 	prefix := in.Prefix
 	if prefix[len(prefix)-1:] != "_" {
 		in.Prefix = fmt.Sprintf("%s_", prefix)
 	}
+
+	// AppDir is required
+	appDirKey := fmt.Sprintf("%sDIR", in.Prefix)
+	appDir := os.Getenv(appDirKey)
+	if appDir == "" {
+		// Default APP_DIR is current working dir
+		var err error
+		appDir, err = os.Getwd()
+		if err != nil {
+			return errors.Errorf("%v env not set%s", appDirKey, "\n")
+		}
+	}
+	in.AppDir = appDir
 
 	return nil
 }
